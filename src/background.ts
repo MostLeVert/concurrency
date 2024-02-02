ad54c6b4a98573cd4afa6fac10be3e4bcde4f9d3
MostLeVert <axeljalonen@gmail.com>
2024-02-02 08:50:32 -0800

    Implement main functionality

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,39 @@
-let active = false;
+const convertButton = document.getElementById(
+    "convert-button"
+) as HTMLDivElement;
+const currencyTypeIn = document.getElementById(
+    "currency-type-in"
+) as HTMLSelectElement;
+const currencyTypeOut = document.getElementById(
+    "currency-type-out"
+) as HTMLSelectElement;
+const currencyAmount = document.getElementById(
+    "currency-amount"
+) as HTMLInputElement;
+const currencyResult = document.getElementById(
+    "currency-result"
+) as HTMLDivElement;
 
-function makeOrange(color: string): void {
-    document.body.style.backgroundColor = color;
-}
-
-chrome.action.onClicked.addListener((tab) => {
-    active = !active;
-    const color = active ? 'orange' : 'white';
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id ? tab.id : -1},
-        func: makeOrange,
-        args: [color]
-    }).then();
+convertButton.addEventListener("click", () => {
+    const base: string = currencyTypeIn.value.toLowerCase();
+    const to: string = currencyTypeOut.value.toLowerCase();
+    const amount: number = Number(currencyAmount.value);
+    getCurrencies(base, to, amount);
 });
+
+function getCurrencies(base: string, to: string, amount: number) {
+    console.log(base, to, amount)
+    console.log(`${base}`);
+    fetch(
+        `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${base}.json`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            const rate: number = Number(data[base][to]);
+            const result: number = amount * rate;
+            currencyResult.innerText = result.toString();
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+}
