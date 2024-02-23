6101493055cfd4303d423d2fc83cf8f847e1cdb4
Axel Jalonen <axeljalonen@gmail.com>
2024-02-23 11:56:09 -0800

    Implement various changes

--- src/page.js
+++ src/page.js
@@ -1,23 +1,10 @@
-const submitButton = document.getElementById("set-default-currency-button");
-const defaultCurrencyInput = document.getElementById(
-  "set-default-currency-input",
-);
-const statusOutput = document.getElementById("status-output");
-
-submitButton.addEventListener("click", function () {
-  statusOutput.innerText = "setting value";
-  setDefaultCurrency(defaultCurrencyInput.value);
-});
-
 function setDefaultCurrency(defaultCurrency) {
-  chrome.storage.local.set(
-    { defaultCurrency: defaultCurrency },
-    function () {
-      if (chrome.runtime.lastError) {
-        statusOutput.innerText = `Error setting default currency: ${chrome.runtime.lastError}`;
-      } else {
-        statusOutput.innerText = `Default currency set to ${defaultCurrency}`;
-      }
-    },
-  );
+  chrome.storage.local.set({ defaultCurrency: defaultCurrency }, function () {
+    if (chrome.runtime.lastError) {
+      statusOutputElement.innerText = `Error setting default currency: ${chrome.runtime.lastError}`;
+    } else {
+      statusOutputElement.innerText = `Default currency set to ${defaultCurrency}`;
+      currentSettingElement.innerText = defaultCurrency;
+    }
+  });
 }
