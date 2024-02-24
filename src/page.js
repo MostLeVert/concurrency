a0752b22be7c192cfe3153297e4c34380f977d41
Axel Jalonen <axeljalonen@gmail.com>
2024-02-24 14:34:07 -0800

    Implement coin search and coin add functionality

--- src/page.js
+++ src/page.js
@@ -1,38 +1,53 @@
-const currencySelectElement = document.getElementById("currencies");
-const statusOutputElement = document.getElementById("status-output");
+const searchResultsE = document.getElementById("search-results");
+const searchResultsES = document.getElementById("search-results-search");
+const currencySelectionEB = document.getElementById("currency-selection");
+const currencySelectionEBS = document.getElementById("currency-selection-S");
+const correncySelectionEBC = document.getElementById("currency-selection-C");
 
-currencySelectElement.addEventListener("input", (e) => {
-  const newDefaultCurrency = e.target.value;
-  chrome.storage.local.set(
-    { defaultCurrency: newDefaultCurrency },
-    function () {
-      if (chrome.runtime.lastError) {
-        statusOutputElement.innerText = `Error setting default currency: ${chrome.runtime.lastError.message}`;
-      } else {
-        statusOutputElement.innerText = `Default currency set to: ${newDefaultCurrency}`;
-      }
-      setTimeout(() => {
-        statusOutputElement.innerText = "Awaiting interaction...";
-      }, 2000);
-    },
-  );
+function toggleElementText(element, textFrom, textTo) {
+  element.innerText = element.innerText === textFrom ? textTo : textFrom;
+}
+function toggleElementDisplay(e) {
+  e.style.display = e.style.display === "block" ? "none" : "block";
+}
+
+currencySelectionEB.addEventListener("click", () => {
+  toggleElementDisplay(searchResultsE);
+  toggleElementText(currencySelectionEBS, "▼", "▲");
 });
-
-fetch(
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json",
-)
+const currencyURL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json";
+fetch(currencyURL)
   .then((response) => response.json())
   .then((data) => {
-    createMenu(data);
+    createData(data);
   })
   .catch(() => {
     statusOutputElement.innerText = "Error fetching data";
   });
-function createMenu(data) {
+function createData(data) {
   for (let key in data) {
-    const selectionOption = document.createElement("option");
-    selectionOption.value = key;
+    const selectionOption = document.createElement("div");
     selectionOption.innerText = key;
-    currencySelectElement.appendChild(selectionOption);
+    selectionOption.classList.add("search-result");
+    selectionOption.addEventListener("click", (e) => {
+      handleCurrencyClick(e);
+    });
+    searchResultsE.appendChild(selectionOption);
   }
+}
+function handleCurrencyClick(e) {
+  const newDefaultCurrency = e.target.innerText;
+  chrome.storage.local.set(
+    { defaultCurrency: newDefaultCurrency },
+    function () {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        correncySelectionEBC.innerText = newDefaultCurrency;
+        toggleElementDisplay(searchResultsE);
+        toggleElementText(currencySelectionEBS, "▼", "▲");
+      }
+    },
+  );
 }
