1f877a1f1d15d29acab370fcff1aee0e37b00b3f
Axel Jalonen <axeljalonen@gmail.com>
2024-02-26 18:14:41 -0800

    Implement conversion popup

--- src/background.js
+++ src/background.js
@@ -9,82 +9,107 @@ const currencyMap = {
   "₿": "btc",
 };
 
-// chrome.contextMenus.onClicked.addListener(genericOnClick);
-// chrome.contextMenus.onClicked.addListener((info, tab) => {
-//   chrome.tabs.sendMessage(tab.id, { action: "spawnDivAtSelection" });
-// });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(["defaultCurrency"], function (result) {
+    const defaultCurrency = result.defaultCurrency
+      ? result.defaultCurrency
+      : "default";
+    chrome.contextMenus.create({
+      id: "convertCurrency",
+      title: `Convert %s to ${defaultCurrency}`,
+      contexts: ["selection"],
+    });
+  });
+});
+
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  if (tab) {
-    /* Inject the code into the current tab */
+  if (info.menuItemId === "convertCurrency") {
     chrome.scripting.executeScript(
       {
         target: { tabId: tab.id },
         files: ["content.js"],
       },
-      (injRes) => {
-        console.log(injRes);
+      () => {
+        chrome.storage.local.get(["defaultCurrency"], async function (result) {
+          const to = result.defaultCurrency.toLowerCase();
+          const from = "usd";
+          const amountIn = Number(info.selectionText);
+          const response = await fetch(
+            `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`,
+          );
+          const data = await response.json();
+          const amountOut = (amountIn * Number(data[to])).toFixed(9).toString();
+          chrome.tabs.sendMessage(
+            tab.id,
+            {
+              action: "injectDiv",
+              from: from,
+              to: to,
+              amountIn: amountIn,
+              amountOut: amountOut,
+            },
+            function (response) {
+              console.log(
+                response ? response.result : "Failed to receive response",
+              );
+            },
+          );
+        });
       },
     );
   }
 });
-async function genericOnClick(info) {
-  let to = "usd";
-  chrome.storage.local.get(["defaultCurrency"], async function (result) {
-    to = result.defaultCurrency || "usd";
-    try {
-      let currencySymbolRegex = /[$€£¥₹Ξ฿₿]/g;
-      let selection = info.selectionText.replace(/,/g, "");
-      const matches = selection.match(currencySymbolRegex);
-      const fromSymbol = matches && matches.length > 0 ? matches[0] : "$";
-      const fromCode = currencyMap[fromSymbol];
-      selection = selection.replace(currencySymbolRegex, "");
-      const amount = Number(selection);
-      if (isNaN(amount)) {
-        console.log("Selected text is not a number.");
-        return;
-      }
-      const converted = await getCurrency(fromCode, to, amount);
-      if (converted) {
-        chrome.storage.local.set(
-          {
-            currencyFromAmount: amount,
-            currencyFrom: fromCode,
-            currencyToAmount: converted,
-            currencyTo: to,
-          },
-          function () {
-            chrome.tabs.create({ url: chrome.runtime.getURL("result.html") });
-          },
-        );
-      } else {
-        console.log("Failed to convert currency.");
-      }
-    } catch (error) {
-      console.log("Error in genericOnClick:", error);
-    }
-  });
-}
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.contextMenus.create({
-    title: `Convert %s to default`,
-    contexts: ["selection"],
-    id: "selection",
-  });
-});
+// async function genericOnClick(info) {
+//   let to = "usd";
+//   chrome.storage.local.get(["defaultCurrency"], async function (result) {
+//     to = result.defaultCurrency || "usd";
+//     try {
+//       let currencySymbolRegex = /[$€£¥₹Ξ฿₿]/g;
+//       let selection = info.selectionText.replace(/,/g, "");
+//       const matches = selection.match(currencySymbolRegex);
+//       const fromSymbol = matches && matches.length > 0 ? matches[0] : "$";
+//       const fromCode = currencyMap[fromSymbol];
+//       selection = selection.replace(currencySymbolRegex, "");
+//       const amount = Number(selection);
+//       if (isNaN(amount)) {
+//         console.log("Selected text is not a number.");
+//         return;
+//       }
+//       const converted = await getCurrency(fromCode, to, amount);
+//       if (converted) {
+//         chrome.storage.local.set(
+//           {
+//             currencyFromAmount: amount,
+//             currencyFrom: fromCode,
+//             currencyToAmount: converted,
+//             currencyTo: to,
+//           },
+//           function () {
+//             chrome.tabs.create({ url: chrome.runtime.getURL("result.html") });
+//           },
+//         );
+//       } else {
+//         console.log("Failed to convert currency.");
+//       }
+//     } catch (error) {
+//       console.log("Error in genericOnClick:", error);
+//     }
+//   });
+// }
 
-async function getCurrency(from, to, amount) {
-  try {
-    const toNew = to.toLowerCase();
-    console.log(from, to);
-    const response = await fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${toNew}.json`,
-    );
-    const data = await response.json();
-    const rate = Number(data[toNew]);
-    const result = amount * rate;
-    return result.toFixed(9).toString();
-  } catch (e) {
-    console.log(e);
-  }
-}
+// async function getCurrency(from, to, amount) {
+//   try {
+//     const toNew = to.toLowerCase();
+//     console.log(from, to);
+//     const response = await fetch(
+//       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${toNew}.json`,
+//     );
+//     const data = await response.json();
+//     const rate = Number(data[toNew]);
+//     const result = amount * rate;
+//     return result.toFixed(9).toString();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
--- src/content.js
+++ src/content.js
@@ -1,15 +1,50 @@
-function createAndAppendDiv() {
-  var d = document.createElement("div");
-  d.setAttribute(
-    "style",
-    "background-color: red; " +
-      "width: 100px; " +
-      "height: 100px; " +
-      "position: fixed; " +
-      "top: 70px; " +
-      "left: 30px; " +
-      "z-index: 9999; ",
-  );
-  document.body.appendChild(d);
+function injectDiv(from, to, amountIn, amountOut, x, y) {
+  // This is a hack to prevent duplicate TODO: FIND ROOT OF DUPLICATE
+  if (document.getElementById("conversionChildElement")) {
+    return;
+  } else {
+    var d = document.createElement("div");
+    d.setAttribute(
+      "style",
+      "background: rgba(255,255,255,.25);" +
+        "position: fixed; " +
+        "padding: 10px;" +
+        "backdrop-filter: blur(10px);" +
+        "border: 2px solid black;" +
+        `top: ${y}px;` +
+        `left: ${x}px;` +
+        "z-index: 9999; ",
+    );
+    d.innerText = `${amountIn} ${from} is ${amountOut} ${to}`;
+    d.id = "conversionChildElement";
+    document.body.appendChild(d);
+  }
 }
-createAndAppendDiv();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "injectDiv") {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const rect = range.getBoundingClientRect();
+    const x = rect.left + window.scrollX; // Adding scrollX for horizontal scroll offset
+    const y = rect.bottom + window.scrollY;
+    injectDiv(
+      request.from,
+      request.to,
+      request.amountIn,
+      request.amountOut,
+      x,
+      y,
+    );
+    sendResponse({ result: "success" });
+  }
+});
+
+// document.addEventListener("mousedown", () => {
+//   document.getElementById("conversionChildElement")?.remove();
+// });
+document.addEventListener("keydown", () => {
+  if (keydown.key === "escape") {
+    document.getElementById("conversionChildElement")?.remove();
+  }
+});
